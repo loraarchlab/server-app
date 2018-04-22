8244210005617d3d03fb02ef83059c04d4b8607a
mzj14 <mzj14@mails.tsinghua.edu.cn>
2018-04-22 15:31:42 +0000

    add tracking route code, need test

--- lorabikeapp/static/js/livemap.js
+++ lorabikeapp/static/js/livemap.js
@@ -6,23 +6,58 @@ $(document).ready(function() {
   const maxDisplayLevel = 19;
   let $la_num = $('#la-num');
   let $lo_num = $('#lo-num');
-  const centralX = parseFloat($la_num.text()).toFixed(6);
-  const centralY = parseFloat($lo_num.text()).toFixed(6);
+  const centralX = parseFloat($la_num.text());
+  const centralY = parseFloat($lo_num.text());
   const centralPoint = new BMap.Point(centralY, centralX);
+  let convertor = new BMap.Convertor();
   // initialize map with central point
   map.centerAndZoom(centralPoint, maxDisplayLevel);
   map.enableScrollWheelZoom(true);
-  // add marker to central point
-  const centralMarker = new BMap.Marker(centralPoint);
-  map.addOverlay(centralMarker);
+  map.addControl(new BMap.NavigationControl());
+  
+  let points = [];
+  
+  /*
+  let setZoom = function(points) {
+    console.log('setZoom');
+    const view = map.getViewport(eval(points));
+    map.centerAndZoom(view.centor, view.zoom);
+  }
+  */
+  let expandRoute = function(data) {
+    const len = data.length;
+    if (len < 2) {
+      return;
+    }
+    const temp = data.slice(len - 2, len);
+    const polyline = new BMap.Polyline(temp, {strokeColor:"red", strokeWeight:2, strokeOpacity:0.5});
+    map.addOverlay(polyline);
+    console.log("expandRoute");
+  }
+
   // start ajax querying
-  // window.setInterval(updatePoints, 1000);
+  let translateCallback = function(data) {
+    if(data.status === 0) {
+      const marker = new BMap.Marker(data.points[0]);
+      map.addOverlay(marker);
+      // const label = new BMap.Label("LoRaBike",{offset:new BMap.Size(20,-10)});
+      // marker.setLabel(label);
+      map.setCenter(data.points[0]);
+      points.push(data.points[0]);
+      expandRoute(points);
+      // setZoom(points);
+    }
+  };
+
   let updateLocation = function() {
-    let pointsData = null;
     $.get("ajax/", function(data){
       $la_num.text(data.latitude);
-      $lo_num.text(data.longitude);
+      $lo_num.text(data.longitude);  
+      convertor.translate([new BMap.Point(data.longitude, data.latitude)],
+                          1, 5, translateCallback);
     });
   };
-  window.setInterval(updateLocation, 1000);
+
+  window.setInterval(updateLocation, 3000);
+
 });
