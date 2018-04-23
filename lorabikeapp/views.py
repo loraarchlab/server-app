7a7bcf7e8a9823451502a2631baab9bc29106350
mzj14 <mzj14@mails.tsinghua.edu.cn>
2018-04-23 01:38:29 +0000

    minor change

--- lorabikeapp/views.py
+++ lorabikeapp/views.py
@@ -17,7 +17,7 @@ def uplink(request):
     received_payload = received_json['data']
     re = struct.unpack('IIdd', base64.b64decode(received_payload))
     # print(re)
-    print(type(re[1]))
+    # print(type(re[1]))
     Location.objects.create(track_time = datetime.datetime.now(),
                             latitude = re[2], longitude = re[3], frame_count = re[1])
     return HttpResponse("Get the post data.")
